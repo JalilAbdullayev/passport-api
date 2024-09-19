6391a83b039a0d0b701e4274cc0114b750932543
JalilAbdullayev <abdullacelil00@gmail.com>
2024-09-19 12:28:09 +0400

    feat(authors): create Authors model, policy, migration, seeder, factory, controller, resource, requests
    - adds API endpoint to get a single author

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,5 +8,6 @@ Route::middleware('auth:api')->prefix('v1')->group(function() {
     Route::get('user', function(Request $request) {
         return $request->user();
     });
+    Route::get('authors/{author}', [AuthorController::class, 'show']);
 });
 
